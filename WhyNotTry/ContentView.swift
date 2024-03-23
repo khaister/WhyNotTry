aee3216bd42bb6672ca1b61f96cdd2df4fd993b4
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-03-23 01:07:20 -0700

    Add slide transition + lint code

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,39 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-  let activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash", "Volleyball", "Badminton", "Climbing", "Hockey", "Kickboxing", "Pickleball", "Skating", "Taichi", "Yoga"]
-  
+  let activities = [
+    "Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf",
+    "Hiking", "Lacrosse", "Rugby", "Squash", "Volleyball", "Badminton", "Climbing", "Hockey",
+    "Kickboxing", "Pickleball", "Skating", "Taichi", "Yoga"
+  ]
+
   let defaultActivity = "Volleyball"
-  
+
   let colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
-  
-  @State private var selected = "Volleyball"
-  @State private var bgColor = Color.blue
-  
+
+  @State private var selectedActivity = "Volleyball"
+  @State private var activityColor = Color.blue
+  @State private var activityId = 1
+
   var body: some View {
     Spacer()
-    
+
     Text("Why not try...")
       .font(.largeTitle.bold())
-    
-    Circle()
-      .fill(bgColor)
-      .padding()
-      .overlay(
-        Image(systemName: "figure.\(selected.lowercased())")
-          .font(.system(size: 144))
-          .foregroundColor(.white)
-      )
-    
-    Text("\(selected)!")
-      .font(.title)
-    
+
+    VStack {
+      Circle()
+        .fill(activityColor)
+        .padding()
+        .overlay(
+          Image(systemName: "figure.\(selectedActivity.lowercased())")
+            .font(.system(size: 144))
+            .foregroundColor(.white)
+        )
+
+      Text("**\(selectedActivity)**").font(.title).foregroundColor(activityColor) + Text(" _!!_")
+        .font(.title)
+    }
+    .transition(.slide)
+    .id(activityId)
+
     Spacer()
-    
+
     Button("Refresh") {
       withAnimation {
-        selected = activities.randomElement() ?? defaultActivity
-        bgColor = colors.randomElement() ?? Color.blue
+        selectedActivity = activities.randomElement() ?? defaultActivity
+        activityColor = colors.randomElement() ?? Color.blue
+        activityId += 1
       }
     }
     .buttonStyle(.borderedProminent)
