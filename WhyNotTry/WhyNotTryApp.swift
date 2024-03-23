5e280640c63579b9e1065261cd60c61988e74f13
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-03-22 18:30:45 -0700

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Khai Nguyen on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
